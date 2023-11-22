ffa80857101c1021151ab31f88fc406f16c7d5e8
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-22 13:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if (year % 4 !== 0) {
+    // Not a leap year
+    return 365;
+  } else if (year % 100 !== 0) {
+    // Leap year (not a century year)
+    return 366;
+  } else if (year % 400 !== 0) {
+    // Century year, not a leap year
+    return 365;
+  } else {
+    // Century year and a leap year
+    return 366;
+  }
+}
+
+// Examples
+console.log(daysOfAYear(2022)); // Output: 365
+console.log(daysOfAYear(2024)); // Output: 366
+console.log(daysOfAYear(1900)); // Output: 365
+console.log(daysOfAYear(2000)); // Output: 366
